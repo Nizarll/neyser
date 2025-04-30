e50f9f840b6b3a16496e77c21238339352f21559
Nizarll <forsubsofmy@gmail.com>
2025-04-30 19:35:50 +0100

    updated support for float and double and allowed for interoperability with c++ whilst also giving an interface template deserialize(buff, size) that takes in the type to deserialize

--- neyser.c
+++ neyser.c
@@ -1,76 +1,98 @@
 #include "neyser.h"
 
-u16 deserialize_u16(i8* buff, size_t size) {
-        if (size < 2) LOG_ERR("error! could not deserialize u16!");
-        return (u16) (u16)(*(buff + 1)) | (u16) (*buff << byte);
+u16 deserialize_u16(i8 *buff, size_t size)
+{
+  if (size < 2) LOG_ERR("error! could not deserialize u16!");
+  return (u16)(u16)(*(buff + 1)) | (u16)(*buff << byte);
 }
 
-u32 deserialize_u32(i8* buff, size_t size) {
-        if (size < 4) LOG_ERR("error! could not deserialize u16!");
-        return (u32) (deserialize_u16(buff + 2, size - 2) | (((u32) deserialize_u16(buff, size)) << 2 * byte));
+u32 deserialize_u32(i8 *buff, size_t size)
+{
+  if (size < 4) LOG_ERR("error! could not deserialize u16!");
+  return (u32)(deserialize_u16(buff + 2, size - 2) | (((u32)deserialize_u16(buff, size)) << 2 * byte));
 }
 
-u64 deserialize_u64(i8* buff, size_t size)  {
-        if (size < 8) LOG_ERR("error! could not deserialize u32!");
-        return (u64) (deserialize_u32(buff + 4, size - 4) | (((u32) deserialize_u32(buff, size)) << 4 * byte));
+u64 deserialize_u64(i8 *buff, size_t size)
+{
+  if (size < 8) LOG_ERR("error! could not deserialize u32!");
+  return (u64)(deserialize_u32(buff + 4, size - 4) | (((u32)deserialize_u32(buff, size)) << 4 * byte));
 }
 
-float deserialize_float(i8* buff, size_t size) {
-    u32 mant = deserialize_u32(buff, size);
-    i8 sign = (u8)(*(buff + 4)) ? -1 : 1;
-    u8 exp = (u8)(*(buff + 5));
-    printf("derialize_float:\n\tmant is %ld, exp is %d, sign is %d\n", mant, exp, sign);
-        if (size < 6) LOG_ERR("error! could not deserialize u64!");
-    return (float)(sign * ldexpf((float)((float)mant / pow(2, 24)), exp));
+float deserialize_float(i8 *buff, size_t size)
+{
+  if (size < 6) LOG_ERR("error! could not deserialize float!");
+
+  u32 mant = deserialize_u32(buff, size);
+  i8 sign = (*(u8 *)(buff + 4)) ? -1 : 1;
+  u8 exp = *(u8 *)(buff + 5);            
+
+  return sign * ldexpf((float)mant / (1 << 24), exp);
 }
 
-void serialize_float(float var, i8* buff, size_t size) {
-        f_pack_t pack = {.sign = var > .0f ? 0 : 1};
-        pack.mant = (u32)(frexpf(var, &pack.exp) * pow(2, 24));
-        printf("serialize_float:\n\tmant is %f, exp is %d, sign is %d\n", pack.mant, pack.exp, pack.sign);
+double deserialize_double(i8 *buff, size_t size)
+{
+  if (size < 8) LOG_ERR("error! could not deserialize double!");
+  u64 raw = deserialize_u64(buff, size);
+  
+  int sign = (raw >> 63) ? -1 : 1;
+  int exponent = ((raw >> 52) & 0x7FF) - 1023;
+  u64 mantissa = raw & 0xFFFFFFFFFFFFFull;
+
+  double frac = (double)mantissa / (1ULL << 52) + 1.0;
+  return sign * ldexp(frac, exponent);
+}
+
+void serialize_float(float var, i8 *buff, size_t size)
+{
+  f_pack_t pack = {.sign = var > .0f ? 0 : 1};
+  pack.mant = (u32)(frexpf(var, &pack.exp) * pow(2, 24));
+  printf("serialize_float:\n\tmant is %f, exp is %d, sign is %d\n", pack.mant, pack.exp, pack.sign);
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        serialize_u32(pack.mant, buff, size);
-        *(buff + 4) = pack.sign;
-        *(buff + 5) = pack.exp;
+  serialize_u32(pack.mant, buff, size);
+  *(buff + 4) = pack.sign;
+  *(buff + 5) = pack.exp;
 #else
-        *buff = pack.sign;
-        *(buff + 1) = pack.exp;
-        serialize_u32(pack.mant, buff + 2, size - 2);
+  *buff = pack.sign;
+  *(buff + 1) = pack.exp;
+  serialize_u32(pack.mant, buff + 2, size - 2);
 #endif
 }
 
-void serialize_u16(u16 var, i8* buff, size_t size) {
-        if (size < 2)
-                LOG_ERR("could not serialize u16 !");
+void serialize_u16(u16 var, i8 *buff, size_t size)
+{
+  if (size < 2)
+    LOG_ERR("could not serialize u16 !");
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        *buff = (var & u16_half_sec) >> 8;
-        *(buff + 1) = var & u16_half_first;
+  *buff = (var & u16_half_sec) >> 8;
+  *(buff + 1) = var & u16_half_first;
 #else
-        *buff = var & u16_half_first;
-        *(buff + 1) = (var & u16_half_sec) >> 8;
+  *buff = var & u16_half_first;
+  *(buff + 1) = (var & u16_half_sec) >> 8;
 #endif
 }
 
-void serialize_u32(u32 var, i8* buff, size_t size) {
-        if (size < 4)
-                LOG_ERR("could not serialize u32 !");
+void serialize_u32(u32 var, i8 *buff, size_t size)
+{
+  if (size < 4)
+    LOG_ERR("could not serialize u32 !");
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        serialize_u16((u16)((var & u32_half_sec) >> (2 * byte)), buff, size);
-        serialize_u16((u16)(var & u32_half_first), buff + 2, size - 2);
+  serialize_u16((u16)((var & u32_half_sec) >> (2 * byte)), buff, size);
+  serialize_u16((u16)(var & u32_half_first), buff + 2, size - 2);
 #else
-        serialize_u16((u16)(var & u32_half_first), buff, size);
-        serialize_u16((u16)((var & u32_half_sec) >> 16), buff + 2, size - 2);
+  serialize_u16((u16)(var & u32_half_first), buff, size);
+  serialize_u16((u16)((var & u32_half_sec) >> 16), buff + 2, size - 2);
 #endif
 }
 
-void serialize_u64(u64 var, i8* buff, size_t size) {
-        if (size < 8)
-                LOG_ERR("could not serialize u64 !");
+void serialize_u64(u64 var, i8 *buff, size_t size)
+{
+  if (size < 8)
+    LOG_ERR("could not serialize u64 !");
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        serialize_u32((u32)((var & u64_half_sec) >> (4 * byte)), buff, size);
-        serialize_u32((u32)(var & u64_half_first), buff + 2, size - 2);
+  serialize_u32((u32)((var & u64_half_sec) >> (4 * byte)), buff, size);
+  serialize_u32((u32)(var & u64_half_first), buff + 2, size - 2);
 #else
-        serialize_u32((u32)(var & u64_half_first), buff, size);
-        serialize_u32((u32)((var & u32_half_sec) >> 32), buff + 2, size - 2);
+  serialize_u32((u32)(var & u64_half_first), buff, size);
+  serialize_u32((u32)((var & u32_half_sec) >> 32), buff + 2, size - 2);
 #endif
 }
--- neyser.h
+++ neyser.h
@@ -17,114 +17,149 @@
 #include <stdio.h>
 
 #ifndef __ORDER_LITTLE_ENDIAN__
-  #define __ORDER_LITTLE_ENDIAN__ 1234
+#define __ORDER_LITTLE_ENDIAN__ 1234
 #endif
 
 #ifndef __ORDER_BIG_ENDIAN__
-  #define __ORDER_BIG_ENDIAN__ 4321
+#define __ORDER_BIG_ENDIAN__ 4321
 #endif
 
 #if defined(MSC_VER)
-	#if REG_DWORD == REG_DWORD_LITTLE_ENDIAN
-    #define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
-	#else
-    #define __BYTE_ORDER__ __ORDER_BIG_ENDIAN__
-	#endif
+#if REG_DWORD == REG_DWORD_LITTLE_ENDIAN
+#define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+#else
+#define __BYTE_ORDER__ __ORDER_BIG_ENDIAN__
+#endif
 #endif
 
-#define ANSI_COLOR_RESET             "\x1b[0m"
-#define ANSI_COLOR_BLACK             "\x1b[30m"
-#define ANSI_COLOR_RED               "\x1b[31m"
-#define ANSI_COLOR_GREEN             "\x1b[32m"
-#define ANSI_COLOR_YELLOW            "\x1b[33m"
-#define ANSI_COLOR_BLUE              "\x1b[34m"
-#define ANSI_COLOR_MAGENTA           "\x1b[35m"
-#define ANSI_COLOR_CYAN              "\x1b[36m"
-#define ANSI_COLOR_WHITE             "\x1b[37m"
-#define ANSI_COLOR_BRIGHT_BLACK      "\x1b[30;1m"
-#define ANSI_COLOR_BRIGHT_RED        "\x1b[31;1m"
-#define ANSI_COLOR_BRIGHT_GREEN      "\x1b[32;1m"
-#define ANSI_COLOR_BRIGHT_YELLOW     "\x1b[33;1m"
-#define ANSI_COLOR_BRIGHT_BLUE       "\x1b[34;1m"
-#define ANSI_COLOR_BRIGHT_MAGENTA    "\x1b[35;1m"
-#define ANSI_COLOR_BRIGHT_CYAN       "\x1b[36;1m"
-#define ANSI_COLOR_BRIGHT_WHITE      "\x1b[37;1m"
-#define ANSI_BG_COLOR_BLACK          "\x1b[40m"
-#define ANSI_BG_COLOR_RED            "\x1b[41m"
-#define ANSI_BG_COLOR_GREEN          "\x1b[42m"
-#define ANSI_BG_COLOR_YELLOW         "\x1b[43m"
-#define ANSI_BG_COLOR_BLUE           "\x1b[44m"
-#define ANSI_BG_COLOR_MAGENTA        "\x1b[45m"
-#define ANSI_BG_COLOR_CYAN           "\x1b[46m"
-#define ANSI_BG_COLOR_WHITE          "\x1b[47m"
-#define ANSI_BG_COLOR_BRIGHT_BLACK   "\x1b[40;1m"
-#define ANSI_BG_COLOR_BRIGHT_RED     "\x1b[41;1m"
-#define ANSI_BG_COLOR_BRIGHT_GREEN   "\x1b[42;1m"
-#define ANSI_BG_COLOR_BRIGHT_YELLOW  "\x1b[43;1m"
-#define ANSI_BG_COLOR_BRIGHT_BLUE    "\x1b[44;1m"
-#define ANSI_BG_COLOR_BRIGHT_MAGENTA "\x1b[45;1m"
-#define ANSI_BG_COLOR_BRIGHT_CYAN    "\x1b[46;1m"
-#define ANSI_BG_COLOR_BRIGHT_WHITE   "\x1b[47;1m"
-#define ANSI_STYLE_BOLD              "\x1b[1m"
-#define ANSI_STYLE_UNDERLINE         "\x1b[4m"
-#define ANSI_STYLE_REVERSED          "\x1b[7m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
 
-#define LOG_OKAY(msg, ...)                                                     \
+#define LOG_OKAY(msg, ...) \
   printf(ANSI_COLOR_GREEN "[✓] | " msg ANSI_COLOR_RESET "\n", ##__VA_ARGS__)
 
-#define LOG_ERR(msg, ...)                                                      \
-  do {                                                                         \
-    printf(ANSI_COLOR_RED "[x] | " msg ANSI_COLOR_RESET "\n", ##__VA_ARGS__);  \
-    exit(1);                                                                   \
+#define LOG_ERR(msg, ...)                                                     \
+  do                                                                          \
+  {                                                                           \
+    printf(ANSI_COLOR_RED "[x] | " msg ANSI_COLOR_RESET "\n", ##__VA_ARGS__); \
+    exit(1);                                                                  \
   } while (0)
 
-#define LOG_WARN(msg, ...)                                                     \
-  printf(ANSI_COLOR_YELLOW "[i] | " msg ANSI_COLOR_RESET "\n", ##__VA_ARGS__)
+#define LOG_WARN(msg, ...) \
+  printf(ANSI_COLOR_YELLOW "[!] | " msg ANSI_COLOR_RESET "\n", ##__VA_ARGS__)
+
+#define deser_inc_i16(buff, size) \
+  deserialize_i16(buff, size);     \
+  buff += 2
+
+#define deser_inc_i32(buff, size) \
+  deserialize_i32(buff, size);     \
+  buff += 4
+
+#define deser_inc_i64(buff, size) \
+  deserialize_i64(buff, size);     \
+  buff += 8
+
+#define deser_inc_f32(buff, size) \
+  deserialize_float(buff, size);   \
+  buff += 8
 
 #define byte 8
 
 #define u16_half_first 0x000000ff
-#define u16_half_sec   0x0000ff00
+#define u16_half_sec 0x0000ff00
 #define u32_half_first 0x0000ffff
-#define u32_half_sec   0xffff0000
+#define u32_half_sec 0xffff0000
 #define u32_half_first 0x0000ffff
-#define u32_half_sec   0xffff0000
+#define u32_half_sec 0xffff0000
 #define u64_half_first 0xffffffff
 
 #define u64_half_sec (UINT64_C(0xffffffff) << 32)
 
-typedef int16_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-typedef uint16_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+  typedef int16_t i8;
+  typedef int16_t i16;
+  typedef int32_t i32;
+  typedef int64_t i64;
 
-void serialize_i16(u16 var, i8* buff, size_t size);
-void serialize_i32(u32 var, i8* buff, size_t size);
-void serialize_i64(u64 var, i8* buff, size_t size);
-void serialize_u16(u16 var, i8* buff, size_t size);
-void serialize_u32(u32 var, i8* buff, size_t size);
-void serialize_u64(u64 var, i8* buff, size_t size);
-void serialize_float(float var, i8* buff, size_t size);
-void serialize_struct(void* data, size_t data_size, i8* buff, size_t buff_size);
+  typedef uint16_t u8;
+  typedef uint16_t u16;
+  typedef uint32_t u32;
+  typedef uint64_t u64;
 
-i16 deserialize_i16(i8* buff, size_t size);
-i32 deserialize_i32(i8* buff, size_t size);
-i64 deserialize_i64(i8* buff, size_t size);
-u16 deserialize_u16(i8* buff, size_t size);
-u32 deserialize_u32(i8* buff, size_t size);
-u64 deserialize_u64(i8* buff, size_t size);
-float deserialize_float(i8* buff, size_t size);
-char* derialize_struct(i8* buff, size_t size); // type-punning to return the base struct
+  void serialize_i16(u16 var, i8 *buff, size_t size);
+  void serialize_i32(u32 var, i8 *buff, size_t size);
+  void serialize_i64(u64 var, i8 *buff, size_t size);
+  void serialize_u16(u16 var, i8 *buff, size_t size);
+  void serialize_u32(u32 var, i8 *buff, size_t size);
+  void serialize_u64(u64 var, i8 *buff, size_t size);
+  void serialize_float(float var, i8 *buff, size_t size);
+  void serialize_struct(void *data, size_t data_size, i8 *buff, size_t buff_size);
 
-typedef struct {
-        u8 sign;    // 1 bit float sign
-        u8 exp;    // 8 bit float exponent
-        u32 mant; // 24 bit float mantissa
-}f_pack_t;
+  i16 deserialize_i16(i8 *buff, size_t size);
+  i32 deserialize_i32(i8 *buff, size_t size);
+  i64 deserialize_i64(i8 *buff, size_t size);
+  u16 deserialize_u16(i8 *buff, size_t size);
+  u32 deserialize_u32(i8 *buff, size_t size);
+  u64 deserialize_u64(i8 *buff, size_t size);
+  float deserialize_float(i8 *buff, size_t size);
+  double deserialize_double(i8 *buff, size_t size);
+
+#ifdef __cplusplus
+
+#if defined(__cpp_concepts) && __cpp_concepts >= 201907L
+
+#include <concepts>
+
+template<typename T>
+concept Deserializable = 
+    std::same_as<T, i16> || std::same_as<T, i32> ||
+    std::same_as<T, i64> || std::same_as<T, u16> ||
+    std::same_as<T, u32> || std::same_as<T, u64> ||
+    std::same_as<T, float> || std::same_as<T, double>;
+
+concept Serializable = requires std::same_as<T, u8> || std::same_as<T, u16>
+template <Serializable T>
+T deserialize(i8* buff, size_t buff_size) {
+  using CleanType = std::remove_cvref_t<T>;
+  if constexpr (std::same_as<CleanType, u16>) return deserialize_u16(buff, buff_size);
+  if constexpr (std::same_as<CleanType, u32>) return deserialize_u32(buff, buff_size);
+  if constexpr (std::same_as<CleanType, u64>) return deserialize_u64(buff, buff_size);
+  if constexpr (std::same_as<CleanType, u16>) return deserialize_u16(buff, buff_size);
+  if constexpr (std::same_as<CleanType, u32>) return deserialize_u32(buff, buff_size);
+  if constexpr (std::same_as<CleanType, u64>) return deserialize_u64(buff, buff_size);
+}
+
+#else
+template<typename T>
+T deserialize_helper(const i8* buff, size_t size);
+template<>
+u8 deserialize_helper<u8>(const i8* buff, size_t size) { return (u8)deserialize_i16((i8*)buff, size); }
+template<>
+u16 deserialize_helper<u16>(const i8* buff, size_t size) { return (u16)deserialize_i16((i8*)buff, size); }
+template<>
+u32 deserialize_helper<u32>(const i8* buff, size_t size) { return (u32)deserialize_i32((i8*)buff, size); }
+template<>
+u64 deserialize_helper<u64>(const i8* buff, size_t size) { return (u64)deserialize_i64((i8*)buff, size); }
+template<>
+float deserialize_helper<float>(const i8* buff, size_t size) { return deserialize_float((i8*)buff, size); }
+template<>
+float deserialize_helper<float>(const i8* buff, size_t size) { return deserialize_float((i8*)buff, size); }
+template<>
+double deserialize_helper<float>(const i8* buff, size_t size) { return deserialize_double((i8*)buff, size); }
+
+template <typename T>
+T deserialize(i8* buff, size_t size) {
+  return deserialize_helper<T>(buff, size);
+}
+#endif
+}
+#endif
 
 #endif // NEYSER_H
